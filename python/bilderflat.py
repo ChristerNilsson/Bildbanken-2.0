f84c06f8f0280c5618412e29c525f96c4302b4c0
ChristerNilsson <janchrister.nilsson@gmail.com>
2022-12-01 01:28:10 +0100

    python

--- python/bilderflat.py
+++ python/bilderflat.py
@@ -1,8 +1,6 @@
-# Om bilder eller kataloger saknas i Small, skapas dessa och Cache uppdateras
-
 import time
 import json
-from os import scandir, mkdir, remove, rmdir
+from os import scandir, mkdir # remove, rmdir
 from os.path import exists, getsize
 from PIL import Image
 import hashlib
@@ -18,26 +16,7 @@ JSON = ROOT + "public\\json\\"
 
 def is_jpg(key): return key.endswith('.jpg') or key.endswith('.JPG')
 
-
 def is_tif(key): return key.endswith('.tif') or key.endswith('.TIF')
-
-
-def frekvens(s):
-	for letter in '-,[]{}.;_"0123456789':
-		s = s.replace(letter,' ')
-	words = s.split(' ')
-	hash = {}
-	for word in words:
-		if word in hash:
-			hash[word] += 1
-		else:
-			hash[word] = 1
-	keys = list(hash.keys())
-	keys.sort()
-	for word in keys:
-		if hash[word] == 1:
-			print(word)
-
 
 def dumpjson(data,f):
 	s = json.dumps(data, ensure_ascii=False, separators=(",", ":"), sort_keys=True)
@@ -47,42 +26,16 @@ def dumpjson(data,f):
 	# frekvens(s)
 	f.write(s)
 
-# def pop(path):
-# 	arr = path.split('\\')
-# 	arr.pop()
-# 	return "\\".join(arr)
-
-# def dump(a):
-# 	for key in a:
-# 		print(key,a[key])
-
-
 def loadJSON(path):
 	if not exists(path): return {}
 	with open(path, 'r', encoding="utf8") as f:
 		return json.loads(f.read())
-
-
-# def pruneCache():
-# 	antal = {'files':0, 'folders':0}
-# 	keys = list(c.keys())
-# 	keys = reversed(keys)
-# 	for key in keys:
-# 		if key not in a:
-# 			if is_jpg(key):
-# 				antal['files'] += 1
-# 			else:
-# 				antal['folders'] += 1
-# 			patch(cache, key, None)
-# 	return antal
-
 
 def ensurePath(root,path):
 	arr = path.split("\\")
 	for i in range(len(arr)):
 		p = root + "\\" + "\\".join(arr[0:i])
 		if not exists(p): mkdir(p)
-
 
 def patch(tree,path,data):
 	arr = path.split("\\")
@@ -94,7 +47,6 @@ def patch(tree,path,data):
 		ptr[key] = data
 	else:
 		del ptr[key]
-
 
 def makeSmall(Original,Home,small,name):
 
@@ -113,19 +65,15 @@ def makeSmall(Original,Home,small,name):
 	bigImg = Image.open(Original+name)
 	bigSize = getsize(Original+name)
 
-	# bigImg.save(Home + "\\" + md5hash + '.jpg')
 	shutil.copyfile(Original + name, Home + "\\" + md5hash + '.jpg')
 
 	smallImg = bigImg.resize((WIDTH, round(WIDTH*bigImg.height/bigImg.width)))
-	# ensurePath(b,name)
 	smallImg.save(small + "\\" + md5hash + '.jpg')
 	lst = [smallImg.width, smallImg.height, bigSize, bigImg.width, bigImg.height, md5hash]
 	md5Register[md5hash] = lst
 	patch(cache, name, lst)
 	return lst
 
-
-# utöka Home, Small och Cache
 def expand(a,d):
 	antal = {'files':0, 'folders':0}
 	for key in a.keys():
@@ -139,8 +87,6 @@ def expand(a,d):
 	print()
 	return antal
 
-
-# minska Cache  # utifrån jmf mellan Original och Cache
 def shrink(d,a):
 	antal = {'files':0, 'folders':0, 'keys':0}
 	keys = list(d.keys())
@@ -148,24 +94,11 @@ def shrink(d,a):
 	for key in keys:
 		if key not in a: # Original
 			patch(cache, key, None)
-			# md5Key = "\\" + d[key][5]+".jpg"
-			# filename = Home + md5Key
-			# if exists(filename):
-			# 	remove(filename)
-			# filename = small + md5Key
-			# if exists(filename):
-			# 	remove(filename)
 			if is_jpg(key):
 				antal['files'] += 1
 			else:
 				antal['folders'] += 1
 	return antal
-
-# def getInfo(path):
-# 	info = Image.open(path)
-# 	size = getsize(path)
-# 	return [info.width, info.height, size]
-
 
 def flat(root, res={}, path=""):
 	ensurePath(root, path)
@@ -181,20 +114,16 @@ def flat(root, res={}, path=""):
 			print("*** Ignored file:", "public\\Home" + path1)
 	return res
 
-
 def flatten(node, res={}, path=''):
 	for key in node:
 		path1 = path + "\\" + key
 		if is_jpg(key):
 			res[path1] = node[key]
-			md5Key = res[path1][5]
-			#md5Register[md5Key] = res[path1]
 		else:
 			res[path1] = ""
 		if not type(node[key]) is list:
 			flatten(node[key],res, path1)
 	return res
-
 
 def compare(a,b,message):
 	res = {}
@@ -212,20 +141,17 @@ def compare(a,b,message):
 		print(message, cfolders, 'folders +', cfiles, 'files')
 	return res
 
-
 def compare2(message,x,y):
 	res = {}
 	res['missing'] = compare(x, y, 'Original vs ' + message + ': missing')
 	res['surplus'] = compare(y, x, 'Original vs ' + message + ': surplus')
 	return res
 
-
 def countFolders(arr):
 	antal = 0
 	for key in arr:
 		if not is_jpg(key): antal += 1
 	return antal
-
 
 hash = {}
 letters = list("+!§()0123456789_,.-¤")
@@ -255,14 +181,6 @@ def convert(hash):
 md5Register = loadJSON(JSON + 'MD5.json') # givet md5key får man listan med sex element
 cache = loadJSON(JSON + 'bilder.json')
 
-# start = time.time()
-# flatWords(cache)
-# keys = convert(hash)
-# print(len(keys),'words in', round(time.time()-start,3),'seconds')
-# print()
-# for [count,key] in keys:
-# 	print(key + ':' + str(count))
-
 a = flat(Original, {}) # Readonly!
 b = flat(Home)   # Används bara för räkning
 c = flat(small)  # Används bara för räkning
@@ -281,16 +199,11 @@ print('Cache:   ', cd, 'folders +', len(d) - cd,'files')
 
 print()
 resCache = compare2('Cache',a,d)
-#resCache = compare2('Cache',a,c)
 
 print()
 for key in resCache['missing'].keys(): print('Cache missing:', key)
 print()
 for key in resCache['surplus'].keys(): print('Cache surplus:', key)
-# print()
-# for key in resCache['missing'].keys(): print('Cache missing:', key)
-# print()
-# for key in resCache['surplus'].keys(): print('Cache surplus:', key)
 
 print()
 update = input('Update? (NO/Yes) ').upper()
@@ -307,13 +220,7 @@ if update:
 	antal = shrink(d,a)
 	if antal['files'] > 0: print('Cache: Deleted', antal['files'], 'files')
 
-	# antal = shrink(d,a,small)
-	# if antal['files'] > 0: print('Small: Deleted', antal['files'], 'files')
-
 	if antal['keys'] > 0: print('Cache: Deleted', antal['keys'], 'keys')
-
-#	antal = pruneCache()
-#	if antal['folders'] > 0 or antal['files'] > 0: print('Cache: Pruned',  antal['folders'], 'folders and', antal['files'], 'files')
 
 	with open(JSON + 'bilder.json', 'w', encoding="utf8") as f: dumpjson(cache,f)
 	with open(JSON + 'MD5.json', 'w', encoding="utf8") as f: dumpjson(md5Register,f)
